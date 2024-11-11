fe75444afb8804efab802694e0c025906796a0fb
dani-ideas <daniideas0@gmail.com>
2024-11-10 22:25:04 -0600

    after funcional programation

--- newVersion/main.py
+++
@@ -1,7 +0,0 @@
-from tkinter import Tk
-from Controllers.Controller_scienceGraficNM import CalculatorController
-
-if __name__ == "__main__":
-    root = Tk()
-    app = CalculatorController(root)
-    root.mainloop()
--- start.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-from test import CalculatorView
-root = tk.Tk()
-app = CalculatorView(root)
-root.mainloop()
